6ca2b7e080c95efda2abe51986b6bf43cd99d837
blurry fish <acht7111020@gmail.com>
2020-08-06 22:57:05 +0800

    Updates

---
+++ precache-manifest.463557a8bc050f220d91e8da8b5bab2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58e00b2dad2fcaeae2d22311be8f75f",
+    "url": "/DSMAP-demo/index.html"
+  },
+  {
+    "revision": "325dca49f459a4289701",
+    "url": "/DSMAP-demo/static/js/2.96a72730.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DSMAP-demo/static/js/2.96a72730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2cae593481f35a03c5",
+    "url": "/DSMAP-demo/static/js/main.a104ddbc.chunk.js"
+  },
+  {
+    "revision": "1a643bd983593791be3f",
+    "url": "/DSMAP-demo/static/js/runtime-main.e0f10ae9.js"
+  }
+]);
